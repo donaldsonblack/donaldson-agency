2b465832b8bfe8c11705c8079969535cec70d523
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-11-14 23:58:12 +1100

    changed handling of gtag

--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -3,28 +3,29 @@
 import Script from "next/script";
 
 export function Analytics() {
-  const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+  // Access environment variable at build time (embedded in the bundle)
+  const measurementId = "G-SXX8QKDWQ0";
   const adsConversionId = "AW-17729509807";
-
-  if (!measurementId) {
-    console.warn("GA Measurement ID not found");
-    return null;
-  }
 
   return (
     <>
+      {/* Load Google Tag Manager script for both GA4 and Google Ads */}
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${adsConversionId}`}
+        strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="lazyOnload">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
+
+          // Configure Google Analytics 4
           gtag('config', '${measurementId}', {
             page_path: window.location.pathname,
           });
+
+          // Configure Google Ads
           gtag('config', '${adsConversionId}');
         `}
       </Script>
