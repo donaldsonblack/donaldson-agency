e8a87cf5feaf98680034ceed3a9c81693806d81e
stirlingwdonaldson <s4169686@student.rmit.edu.au>
2025-11-17 22:30:11 +1100

    optimised website

--- next.config.ts
+++ next.config.ts
@@ -42,6 +42,10 @@ const nextConfig: NextConfig = {
             key: 'Referrer-Policy',
             value: 'origin-when-cross-origin',
           },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
         ],
       },
       {
